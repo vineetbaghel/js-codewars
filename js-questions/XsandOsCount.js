b68e0dc65ad7354bbe10c0f6d9c8ff9b2f1cb1da
VineetbaghelGit <vineetb.official03@gmail.com>
2023-09-11 08:55:15 +0530

    minor change

--- js-questions/XsandOsCount.js
+++ js-questions/XsandOsCount.js
@@ -1,18 +1,21 @@
 // DESCRIPTION:
 // Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
 
-const XO = (str) => {
+const str = "zpzpAAppxxoo";
+
+const XO1 = (str) => {
   str = str.toLowerCase().split("");
   return (
     str.filter((x) => x === "x").length === str.filter((x) => x === "o").length
   );
 };
-function XO(str) {
+console.log("Answer --->", XO1(str));
+
+function XO2(str) {
   const arr = str.toLowerCase().split("");
   let xcount = 0;
   let ocount = 0;
   arr.forEach((data) => {
-    console.log(data);
     if (data === "x") {
       xcount++;
     }
@@ -25,9 +28,11 @@ function XO(str) {
   }
   return false;
 }
+console.log("Answer --->", XO2(str));
 
-function XO(str) {
+function XO3(str) {
   return (
     str.toLowerCase().split("x").length === str.toLowerCase().split("o").length
   );
 }
+console.log("Answer --->", XO3(str));
